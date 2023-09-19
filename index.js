e7e7ed9cd538598df4c10378d1eab4a33edb94f6
Andrew <93514394+JustRey-007@users.noreply.github.com>
2023-09-19 12:47:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ let reloadWeb = document.getElementById('img_on_not');
 reloadWeb.addEventListener('click', function() {
     location.reload(true);
 });
+
+let textarea = document.getElementById('note-input');
+let buttonSave = document.getElementById('save__button');
+let notesList = document.getElementById('notes-list');
+
+function saveNote() {
+    let noteText = textarea.value;
+
+    let listItem = document.createElement('li');
+    
+    let link = document.createElement('a');
+    link.href = "#";
+    link.textContent = "Заметка";
+
+    listItem.textContent = noteText;
+    
+    listItem.appendChild(link);
+
+    notesList.appendChild(listItem);
+
+    textarea.value = '';
+}
+
+buttonSave.addEventListener('click', saveNote);
